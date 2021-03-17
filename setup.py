bdbf0b6007149d354007f575cee5b51abaa46030
Richard Neumann <r.neumann@homeinfo.de>
2021-03-17 11:11:22 +0100

    Migrated to setuptools

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 #! /usr/bin/env python3
+"""Install script."""
 
-from distutils.core import setup
+from setuptools import setup
+
 
 setup(
     name='is24csv',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
     author_email='info@homeinfo.de',
     maintainer='Richard Neumann',
     maintainer_email='r.neumann@homeinfo.de',
     packages=['is24csv'],
-    description=('Immobilienscout24 legacy CSV library.'))
+    description=('Immobilienscout24 legacy CSV library.')
+)
